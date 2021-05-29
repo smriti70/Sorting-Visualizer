36557da13b7fd7fe19edbb7e316a4418f29b7194
Smriti Singh <smriti2815@gmail.com>
2021-05-29 16:01:29 +0530

    Add speed scroller

--- script.js
+++ script.js
@@ -13,16 +13,18 @@ document.getElementById("bubble-sort").addEventListener("click",function(){
     bubbleSort(bars);
 });
 
-let bubbleSort = (inputArr) => {
+let bubbleSort = async(inputArr) => {
     const len = inputArr.length;
     for (let i = 0; i < len; i++) {
         for (let j = 0; j < len-i-1; j++) {
             let val1 = parseInt(inputArr[j].style.height); 
             let val2 = parseInt(inputArr[j+1].style.height);
+            var speed=document.querySelector("#speed");
+            var speedOfSorting=speed.value;
             if (val1 > val2) {
                 inputArr[j].style.background="red";
                 inputArr[j+1].style.background="red";
-
+                await new Promise(resolve => setTimeout(() => {resolve()},speedOfSorting));
                 swapIt(inputArr[j],inputArr[j+1]);
 
                 inputArr[j].style.background="green";
